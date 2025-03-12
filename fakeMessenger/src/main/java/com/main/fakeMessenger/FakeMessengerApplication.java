b4303377877c2873cf624aa02ae0d64efabdef53
nguyen thu <nguyenthu180100@gmail.com>
2025-03-12 22:07:20 +0700

    init

---
+++ fakeMessenger/src/main/java/com/main/fakeMessenger/FakeMessengerApplication.java
@@ -0,0 +1,13 @@
+package com.main.fakeMessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeMessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeMessengerApplication.class, args);
+	}
+
+}
---
+++ fakeMessenger/src/test/java/com/main/fakeMessenger/FakeMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.fakeMessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeMessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
